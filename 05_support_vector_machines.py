b8730ceb10a54eef738f15eef246accaf5e8ef34
brown5628 <brown5628@gmail.com>
2020-04-26 06:51:11 -0400

    complete ch 5

---
+++ 05_support_vector_machines.py
@@ -0,0 +1,64 @@
+# %%
+import numpy as np
+from sklearn import datasets
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+from sklearn.datasets import make_moons
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.svm import SVC
+from sklearn.svm import LinearSVR
+from sklearn.svm import SVR
+
+# %%
+iris = datasets.load_iris()
+X = iris["data"][:, (2, 3)]
+y = (iris["target"] == 2).astype(np.float64)
+
+svm_clf = Pipeline(
+    [("scaler", StandardScaler()), ("linear_scv", LinearSVC(C=1, loss="hinge"))]
+)
+
+svm_clf.fit(X, y)
+
+# %%
+svm_clf.predict([[5.5, 1.7]])
+
+# %%
+X, y = make_moons(n_samples=100, noise=0.15)
+polynomial_svm_clf = Pipeline(
+    [
+        ("poly_features", PolynomialFeatures(degree=3)),
+        ("scaler", StandardScaler()),
+        ("svm_clf", LinearSVC(C=10, loss="hinge")),
+    ]
+)
+
+polynomial_svm_clf.fit(X, y)
+
+# %%
+poly_kernel_svm_clf = Pipeline(
+    [
+        ("scaler", StandardScaler()),
+        ("svm_clf", SVC(kernel="poly", degree=3, coef0=1, C=5)),
+    ]
+)
+
+poly_kernel_svm_clf.fit(X, y)
+
+# %%
+rbf_kernel_svm_clf = Pipeline(
+    [("sclaer", StandardScaler()), ("svm_clf", SVC(kernel="rbf", gamma=5, C=0.001))]
+)
+rbf_kernel_svm_clf.fit(X, y)
+
+
+# %%
+svm_reg = LinearSVR(epsilon=1.5)
+svm_reg.fit(X, y)
+
+# %%
+svm_poly_reg = SVR(kernel="poly", degree=2, C=100, epsilon=0.1)
+svm_poly_reg.fit(X, y)
+
+# %%

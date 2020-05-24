2caa573e9f0ce71a1829a46a1a5563afe57df241
Will Brown <brown5628@gmail.com>
2020-05-24 11:15:40 -0400

    begin code of ch 10

---
+++ 10_intro_to_ann.py
@@ -0,0 +1,27 @@
+# %%
+import sys
+
+# import tensorflow as tf 
+import numpy as np 
+import os
+import matplotlib as mpl 
+import matplotlib.pyplot as plt 
+import sklearn
+from sklearn.datasets import load_iris 
+from sklearn.linear_model import Perceptron
+
+# %%
+iris = load_iris()
+X = iris.data[:, (2,3)]
+y = (iris.target == 0).astype(np.int)
+
+per_clf = Perceptron(max_iter=1000, tol=1e-3, random_state=42)
+per_clf.fit(X, y)
+
+y_pred = per_clf.predict([[2, .5]])
+
+# %%
+y_pred 
+
+
+# %%
